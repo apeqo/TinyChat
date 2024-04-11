485f2737487b8c5b11eca3f05c99cb2addb706ca
apeqo <73530569+apeqo@users.noreply.github.com>
2024-04-11 17:19:29 +0800

    Add files via upload

---
+++ mainwindow.h
@@ -0,0 +1,36 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <logindialog.h>
+#include <registerdialog.h>
+/******************************************************************************
+ *
+ * @file       mainwindow.h
+ * @brief      主窗口
+ *
+ * @author     wwy
+ * @date       2024/04/11
+ * @history
+ *****************************************************************************/
+QT_BEGIN_NAMESPACE
+namespace Ui {
+class MainWindow;
+}
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+public slots:
+    void SlotSwitchReg();
+private:
+    Ui::MainWindow *ui;
+    LoginDialog *_login_dialog;
+    RegisterDialog *_reg_dialog;
+};
+#endif // MAINWINDOW_H
